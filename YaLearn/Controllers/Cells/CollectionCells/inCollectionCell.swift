88c2aa3d321d176e52af833d42457c488dcb3b10
ThabetRebhi <thabet.rebhi@gmail.com>
2020-03-02 16:26:42 +0100

    add collectionItem

---
+++ YaLearn/Controllers/Cells/CollectionCells/inCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  inCollectionCell.swift
+//  YaLearn
+//
+//  Created by Thabet on 3/2/20.
+//  Copyright © 2020 Thabet. All rights reserved.
+//
+
+import UIKit
+
+class inCollectionCell : UICollectionViewCell {
+   
+    @IBOutlet weak var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
