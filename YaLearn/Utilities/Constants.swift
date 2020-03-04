cb9e3c67df712c8c8aac2175383f4443b2c3ef16
ThabetRebhi <thabet.rebhi@gmail.com>
2020-03-04 11:10:05 +0100

    remove unused constants

--- YaLearn/Utilities/Constants.swift
+++ YaLearn/Utilities/Constants.swift
@@ -19,57 +19,60 @@ struct Constants {
     
     struct ViewControllers {
         
+        static let PDFReader = "PDFReader" //USED
+        
+        
         //launch page
-        static let launchVC = "LaunchVC"
-        
-        //login pages
-        static let loginVC = "SignInVC"
-        static let registerVC = "SignUpVC"
-        static let changePwdVC = "ChangePasswordVC"
-        static let forgotPwdVC = "ForgotPasswordVC"
-        static let updateProfileVC = "UpdateProfileVC"
-        static let whoIsWatchingVC = "WhoIsWatchingVC"
-        
-        //baseVC
-        static let baseVC = "BaseTabBarController"
-        
-        //tabbar main pages
-        static let homeVC = "HomeVC"
-        static let searchVC = "SearchVC"
-        static let categoriesVC = "CategoriesVC"
-        static let downloadVC = "DownloadsVC"
-        static let moreVC = "MoreVC"
-        
-        //Single video page
-        static let singleVC = "SingleVideoVC"
-        static let playerVC = "PlayerVC"
-        
-        //****Tabbar Sub pages****
-        //SettingsSub
-        static let manageProVC = "ManageProVC"
-        static let editProfileVC = "EditProfileVC"
-        static let appSettingsVC = "AppSettingsVC"
-        static let staticPagesVC = "StaticPagesVC"
-        static let accountVC = "AccountVC"
-        static let viewPlansVC = "ViewPlansVC"
-        static let viewPlanDetailsVC = "ViewPlanDetailsVC"
-        static let paymentsVC = "PaymentsVC"
-        static let invoiceVC = "InvoiceVC"
-        static let myPlansVC = "MyPlansVC"
-        static let myPlanDetailsVC = "MyPlanDetailsVC"
-        static let cardsVC = "CardsVC"
-        static let videosListCommonVC = "VideosListCommonVC"
-        static let paidVideosVC = "PaidVideosVC"
-        static let editAccountVC = "EditAccountVC"
-        static let notificationVC = "NotificationVC"
-        
-        // SeeAll page
-        static let seeAllVC = "SeeAllVC"
-        
-        // Category detail
-        static let categoryDetailedVC = "CategoryDetailedVC"
+//        static let launchVC = "LaunchVC"
+//
+//        //login pages
+//        static let loginVC = "SignInVC"
+//        static let registerVC = "SignUpVC"
+//        static let changePwdVC = "ChangePasswordVC"
+//        static let forgotPwdVC = "ForgotPasswordVC"
+//        static let updateProfileVC = "UpdateProfileVC"
+//        static let whoIsWatchingVC = "WhoIsWatchingVC"
+//
+//        //baseVC
+//        static let baseVC = "BaseTabBarController"
+//
+//        //tabbar main pages
+//        static let homeVC = "HomeVC"
+//        static let searchVC = "SearchVC"
+//        static let categoriesVC = "CategoriesVC"
+//        static let downloadVC = "DownloadsVC"
+//        static let moreVC = "MoreVC"
+//
+//        //Single video page
+//        static let singleVC = "SingleVideoVC"
+//        static let playerVC = "PlayerVC"
+//
+//        //****Tabbar Sub pages****
+//        //SettingsSub
+//        static let manageProVC = "ManageProVC"
+//        static let editProfileVC = "EditProfileVC"
+//        static let appSettingsVC = "AppSettingsVC"
+//        static let staticPagesVC = "StaticPagesVC"
+//        static let accountVC = "AccountVC"
+//        static let viewPlansVC = "ViewPlansVC"
+//        static let viewPlanDetailsVC = "ViewPlanDetailsVC"
+//        static let paymentsVC = "PaymentsVC"
+//        static let invoiceVC = "InvoiceVC"
+//        static let myPlansVC = "MyPlansVC"
+//        static let myPlanDetailsVC = "MyPlanDetailsVC"
+//        static let cardsVC = "CardsVC"
+//        static let videosListCommonVC = "VideosListCommonVC"
+//        static let paidVideosVC = "PaidVideosVC"
+//        static let editAccountVC = "EditAccountVC"
+//        static let notificationVC = "NotificationVC"
+//
+//        // SeeAll page
+//        static let seeAllVC = "SeeAllVC"
+//
+//        // Category detail
+//        static let categoryDetailedVC = "CategoryDetailedVC"
     }
-    
+
     struct Cells {
         
         //TableView cells
@@ -77,35 +80,42 @@ struct Constants {
         static let HomeCollectionCell = "HomeCollectionCell" //USED
         static let inCollectionCell = "inCollectionCell" //USED
         
-        static let moreCell = "MoreCell"
-        static let appSettingsCell = "AppSettingsCell"
-        static let accountCell = "AccountCell"
-        static let viewPlansCell = "ViewPlansCell"
-        static let myPlansCell = "MyPlansCell"
-        static let cardsCell = "CardsCell"
-        static let homeBannerTCell = "HomeBannerCell"
-        static let homeCommonCell = "HomeCommonCell"
-        static let videosListTCell = "VideosListCommonCell"
-        static let notificationCell = "NotificationCell"
+//        static let moreCell = "MoreCell"
+//        static let appSettingsCell = "AppSettingsCell"
+//        static let accountCell = "AccountCell"
+//        static let viewPlansCell = "ViewPlansCell"
+//        static let myPlansCell = "MyPlansCell"
+//        static let cardsCell = "CardsCell"
+//        static let homeBannerTCell = "HomeBannerCell"
+//        static let homeCommonCell = "HomeCommonCell"
+//        static let videosListTCell = "VideosListCommonCell"
+//        static let notificationCell = "NotificationCell"
+//
+//        //Single video table cells:
+//        static let playerTableCell = "PlayerTableCell"
+//        static let detailedTableCell = "DetailedTableCell"
+//        static let utilityTableCell = "UtilityTableCell"
+//        static let trailerTableCell = "TrailersTableCell"
+//        static let seasonTableCell = "SeasonTableCell"
+//        static let moreLikeThisTableCell = "MoreLikeThisTableCell"
+//
+//        //CollectionView cell
+//        static let moreCCell = "MoreCCell"
+//        static let manageCCell = "ManageCCell"
         
-        //Single video table cells:
-        static let playerTableCell = "PlayerTableCell"
-        static let detailedTableCell = "DetailedTableCell"
-        static let utilityTableCell = "UtilityTableCell"
-        static let trailerTableCell = "TrailersTableCell"
-        static let seasonTableCell = "SeasonTableCell"
-        static let moreLikeThisTableCell = "MoreLikeThisTableCell"
+//        static let homeDefaultCCell = "HomeDefaultCollectionCell"
         
-        //CollectionView cell
-        static let moreCCell = "MoreCCell"
-        static let manageCCell = "ManageCCell"
+//        static let categoryCollectionCell = "CategoriesCollectionCell"
+//        static let seeaAllCCell = "SeeAllCollectionCell"
+//        static let whoIsWatchingCCell = "WhoIsWatchingCCell"
+//        static let homeBannerCCell = "HomeBannerCollectionCell"
+    }
+    
+    struct StoryboardIds {
         
-        static let homeDefaultCCell = "HomeDefaultCollectionCell"//USED
-        
-        static let categoryCollectionCell = "CategoriesCollectionCell"
-        static let seeaAllCCell = "SeeAllCollectionCell"
-        static let whoIsWatchingCCell = "WhoIsWatchingCCell"
-        static let homeBannerCCell = "HomeBannerCollectionCell"
+        static let mainSb = "Main"//USED
+        static let loginSb = "Login"//USED
+        static let moreStories = "MoreStories"//USED
     }
     
 }
